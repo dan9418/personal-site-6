cd472615fdb3d5a349d62970986f4a3939f48496
Dan Bednarczyk <dpbednarczyk@gmail.com>
2023-10-30 11:04:40 -0400

    basic photo grid

--- src/app/portfolio/PortfolioPage.tsx
+++ src/app/portfolio/PortfolioPage.tsx
@@ -1,11 +1,300 @@
 import PageHeader from "@/components/PageHeader";
+import Image from "next/image";
+
+const PORTFOLIO_CONFIG = [
+    {
+        name: 'Album Covers',
+        description: 'Artwork for various musical projects',
+        images: [
+            {
+                name: 'Silent City (Front)',
+                year: 2017,
+                caption: '',
+                path: '/img/albums/silent_city.jpg'
+            },
+            {
+                name: 'Silent City (Back)',
+                year: 2017,
+                caption: '',
+                path: '/img/albums/silent_city_back.jpg'
+            },
+            {
+                name: 'Need Not',
+                year: 2018,
+                caption: '',
+                path: '/img/albums/need_not.jpg'
+            },
+            {
+                name: 'Peanut Butter Jam',
+                year: 2016,
+                caption: '',
+                path: '/img/albums/peanut_butter_jam.jpg'
+            },
+            {
+                name: 'Songs; And How NOT To Write Them! (Front)',
+                year: 2015,
+                caption: '',
+                path: '/img/albums/songs_and_how_not_to_write_them.jpg'
+            },
+            {
+                name: 'Songs; And How NOT To Write Them! (Back)',
+                year: 2015,
+                caption: '',
+                path: '/img/albums/songs_and_how_not_to_write_them_back.jpg'
+            },
+            {
+                name: 'Yellow Snow',
+                year: 2015,
+                caption: '',
+                path: '/img/albums/yellow_snow.jpg'
+            },
+            {
+                name: 'Human Architecture',
+                year: 2017,
+                caption: '',
+                path: '/img/albums/human_architecture.jpg'
+            },
+            {
+                name: 'Mac DeMarco Covers (Parody)',
+                year: 2017,
+                caption: '',
+                path: '/img/albums/mac_demarco_covers.jpg'
+            },
+            {
+                name: 'Out Of Reach',
+                year: 2011,
+                caption: '',
+                path: '/img/albums/out_of_reach.jpg'
+            },
+            {
+                name: 'String',
+                year: 2017,
+                caption: '',
+                path: '/img/albums/string.jpg'
+            },
+            {
+                name: 'Of Dreams',
+                year: 2017,
+                caption: '',
+                path: '/img/albums/of_dreams.jpg'
+            }
+        ]
+    },
+    {
+        name: 'Snapchat Filters',
+        description: 'Some Snapchat geofilters I designed in college',
+        images: [ // 4
+            {
+                name: 'Chesterfield Street',
+                year: 2015,
+                caption: '',
+                path: '/img/filters/chesterfield.png'
+            },
+            {
+                name: 'WPTS Radio',
+                year: 2015,
+                caption: '',
+                path: '/img/filters/wpts.png'
+            },
+            {
+                name: 'Ward Street',
+                year: 2016,
+                caption: '',
+                path: '/img/filters/ward.png'
+            },
+            {
+                name: 'Pitt Residence Halls',
+                year: 2015,
+                caption: '',
+                path: '/img/filters/pitt.jpg'
+            }
+        ]
+    },
+    {
+        name: 'Just For Fun',
+        description: 'Some fun pieces indulging my goofy sense of humor',
+        images: [ //14
+            {
+                name: 'Angels & Demons',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/angel_demon.jpg'
+            },
+            {
+                name: 'CEO Bednarczyk',
+                year: 2013,
+                caption: '',
+                path: '/img/personal/ceo.jpg'
+            },
+            {
+                name: 'Stalemate',
+                year: 2017,
+                caption: '',
+                path: '/img/personal/chess.jpg'
+            },
+            {
+                name: 'Dolla Dolla Bill Y\'all',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/dollar.jpg'
+            },
+            {
+                name: 'The Godfather Parody',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/godfather.jpg'
+            },
+            {
+                name: 'Hostage',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/hostage.jpg'
+            },
+            {
+                name: 'My Good Friend Jimmy',
+                year: 2014,
+                caption: '',
+                path: '/img/personal/jimmy_fallon.jpg'
+            },
+            {
+                name: 'MiniMe',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/minime.jpg'
+            },
+            {
+                name: 'Have You Seen Me?',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/missing_fridge.jpg'
+            },
+            {
+                name: 'Multitasking',
+                year: 2009,
+                caption: '',
+                path: '/img/personal/multitasking.jpg'
+            },
+            {
+                name: 'The Simpsons Parody',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/simpsons.jpg'
+            },
+            {
+                name: 'Skydiving',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/skydiving.jpg'
+            },
+            {
+                name: 'Painted (Green Day Parody)',
+                year: 2011,
+                caption: '',
+                path: '/img/personal/spray_paint.jpg'
+            },
+            {
+                name: 'Digital World',
+                year: 2012,
+                caption: '',
+                path: '/img/personal/tv.jpg'
+            }
+        ]
+    },
+    {
+        name: 'T-Shirts',
+        description: 'Some shirts I designed in college',
+        images: [ // 5
+            {
+                name: 'E-Week: Harry Potter',
+                year: 2014,
+                caption: '',
+                path: '/img/shirts/harry_potter.jpg'
+            },
+            {
+                name: 'E-Week: Hunger Games',
+                year: 2015,
+                caption: '',
+                path: '/img/shirts/hunger_games.jpg'
+            },
+            {
+                name: 'E-Week: Freshmen Crest',
+                year: 2014,
+                caption: '',
+                path: '/img/shirts/freshmen.jpg'
+            },
+            {
+                name: 'E-Week: Pixar',
+                year: 2017,
+                caption: '',
+                path: '/img/shirts/pixar.jpg'
+            },
+            {
+                name: 'The Panther Pitt',
+                year: 2016,
+                caption: '',
+                path: '/img/shirts/panther_pitt.jpg'
+            }
+        ]
+    },
+    {
+        name: 'Tattoos',
+        description: 'Some tattoos I designed',
+        images: [ // 4
+            {
+                name: 'Nature Arm Band',
+                year: 2017,
+                caption: '',
+                path: '/img/tattoos/lost_is_found.jpg'
+            },
+            {
+                name: 'Harmonic Series',
+                year: 2019,
+                caption: '',
+                path: '/img/tattoos/harmonic_series.png'
+            },
+            {
+                name: 'Binary Cross',
+                year: 2015,
+                caption: '',
+                path: '/img/tattoos/binary_cross.png'
+            },
+            {
+                name: 'Bike',
+                year: 2018,
+                caption: '',
+                path: '/img/tattoos/bike.png'
+            }
+        ]
+    }
+] as const;
+
 
 const PortfolioPage = () => {
     return (
         <>
             <PageHeader text='Creative Portfolio' />
+            <p>
+                DISCLAIMER: This page is still under construction! I am planning to add more content and higher resolution images.
+            </p>
+            {PORTFOLIO_CONFIG.map(section => {
+                const { name, description, images } = section;
+                return (<>
+                    <h2 className="text-lg">{name}</h2>
+                    <p className="my-4">{description}</p>
+                    <ul className="grid grid-cols-3">
+                        {images.map(img => (
+                            <li key={img.path}>
+                                <Image src={img.path} alt={img.name} width={256} height={256}/>
+                            </li>
+                        ))}
+                    </ul>
+                </>);
+            })}
         </>
     )
 }
+
+
+
 
 export default PortfolioPage
