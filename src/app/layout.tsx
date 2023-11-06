6c53f3a48b7cb125a3976a86496141472be8a2af
Dan Bednarczyk <dpbednarczyk@gmail.com>
2023-11-06 18:19:27 -0500

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Dan Bednarczyk',
   description: 'Technical resume, creative portfolio, and contact information',
+  // icons: {
+  //   icon: '/favicon.ico',
+  // },
 }
 
 export default function RootLayout({
