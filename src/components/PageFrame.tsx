c0f349357a323b032982fefdcc60d5f78b0dd233
Dan Bednarczyk <dpbednarczyk@gmail.com>
2023-11-17 17:33:18 -0500

    bg gradient

--- src/components/PageFrame.tsx
+++ src/components/PageFrame.tsx
@@ -6,7 +6,7 @@ const PageFrame: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <>
             <Nav />
-            <main className="min-h-screen theme-l-bg-2 flex justify-center box-border shadow-xl">
+            <main className="min-h-screen theme-bg-gradient flex justify-center box-border shadow-xl">
                 <div className="w-full max-w-5xl py-16 lg:py-24 px-4 lg:px-8 theme-l-bg-1 min-h-screen">
                     {children}
                 </div>
