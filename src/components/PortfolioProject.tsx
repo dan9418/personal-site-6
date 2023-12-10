2eed21ee8f5c4799e6b6879441dc626f3574edc8
Dan Bednarczyk <dpbednarczyk@gmail.com>
2023-12-10 13:18:45 -0500

    improve thumbnail view

--- src/components/PortfolioProject.tsx
+++ src/components/PortfolioProject.tsx
@@ -12,22 +12,22 @@ interface PortfolioProjectProps {
 
 const PortfolioProject: FC<PortfolioProjectProps> = ({ title, description, imgSrc, href }) => {
     return (
-        <SmartLink href={href} className="block p-2 tablet:p-4 hover:bg-slate-100 rounded-lg">
-            <div className="grid grid-cols-[auto_1fr] gap-4 items-start">
-                <div>
+        <SmartLink href={href} className="block h-full p-2 tablet:p-4 hover:bg-slate-100 rounded-lg border theme-d-border mb-8">
+            <div className="">
+                <div className="h-[256px] w-[256px] overflow-hidden bg-white rounded flex items-center justify-center">
                     <Image
                         src={imgSrc}
                         alt={title}
                         width={128}
                         height={128}
                         sizes="(max-width: 767px) 100vw, (max-width: 1023px) 50vw, 33vw"
-                        className="w-[96px] tablet:w-[128px] desktop:w-[256px]"
+                        className="w-[256px]"
                     />
                 </div>
-                <div>
-                    <PageHeader text={title} level='h4' />
+                <div className="">
+                    <PageHeader text={title} level='h4' className="mt-2" />
                     {description && (
-                        <p className="text-sm tablet:text-lg">
+                        <p className="text-sm  theme-l-fg-2">
                             {description}
                         </p>
                     )}
@@ -39,7 +39,7 @@ const PortfolioProject: FC<PortfolioProjectProps> = ({ title, description, imgSr
 
 const PortfolioProjectList: FC<{ items: PortfolioProjectProps[] }> = ({ items }) => {
     return (
-        <ul className="flex flex-col gap-4" >
+        <ul className="grid tablet:grid-cols-2 desktop:grid-cols-3 gap-4" >
             {items.map(item =>
                 <li key={item.title}>
                     <PortfolioProject {...item} />
